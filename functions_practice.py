1cb38fd49b99f70004a0b8264eb0d294bef32683
Daniel Eastwood <91645351+Deastwood83@users.noreply.github.com>
2022-07-06 19:10:29 -0400

    Add files via upload

--- functions_practice.py
+++ functions_practice.py
@@ -5,10 +5,10 @@ def pack(a, b, c):
     return [a, b, c]
 
 def eat_lunch(my_list):
-    if lunch(my_list) == 0:
+    if len(my_list) == 0:
         print("My lunchbox is empty!")
-        else:
-            for i in range(lunch(my_list)):
+    else:
+        for i in range(len(my_list)):
                 if i == 0:
                     print(f"First I eat {my_list[0]}")
                 else:
